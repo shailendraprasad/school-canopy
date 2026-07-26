b54150bf881b42630ddf14b78b353c8aca50bba5
Shailendra Prasad <n.shailendraprasad@gmail.com>
2026-07-26 13:32:39 +0530

    Update environment.staging.ts

--- platform-admin/src/environments/environment.staging.ts
+++ platform-admin/src/environments/environment.staging.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   staging: true,
-  apiUrl: 'https://school-canopy-api-461575837576.asia-south1.run.app'  // Replace with your Cloud Run staging URL
+  apiUrl: 'https://school-canopy-staging-api-487857134790.asia-south1.run.app'
 };
