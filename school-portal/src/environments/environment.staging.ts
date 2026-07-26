e7474887fe581a63bd93d9a972f1cd3a2fae2a0c
Shailendra Prasad <n.shailendraprasad@gmail.com>
2026-07-26 14:09:39 +0530

    Update environment.staging.ts

--- school-portal/src/environments/environment.staging.ts
+++ school-portal/src/environments/environment.staging.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   staging: true,
-  apiUrl: 'https://school-canopy-staging-api-487857134790.asia-south1.run.app'
+  apiUrl: 'https://school-canopy-staging-api-487857134790.asia-south1.run.app',
   parentPortalUrl: 'https://school-canopy-parent.web.app'
 };
